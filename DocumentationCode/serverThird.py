3b52a25656f8ed266c8c83c4cac89ee68cd5b5e3
Mateusz Rzeczyca <mateusz_rz@poczta.fm>
2018-10-23 13:19:39 +0200

    addedDocs

---
+++ DocumentationCode/serverThird.py
@@ -0,0 +1,31 @@
+import io
+import socket
+import struct
+import time
+import picamera
+
+client_socket = socket.socket()
+client_socket.connect(('my_server', 8000))
+connection = client_socket.makefile('wb')
+try:
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        camera.framerate = 30
+        time.sleep(2)
+        start = time.time()
+        stream = io.BytesIO()
+        # Use the video-port for captures...
+        for foo in camera.capture_continuous(stream, 'jpeg',
+                                             use_video_port=True):
+            connection.write(struct.pack('<L', stream.tell()))
+            connection.flush()
+            stream.seek(0)
+            connection.write(stream.read())
+            if time.time() - start > 30:
+                break
+            stream.seek(0)
+            stream.truncate()
+    connection.write(struct.pack('<L', 0))
+finally:
+    connection.close()
+    client_socket.close()
